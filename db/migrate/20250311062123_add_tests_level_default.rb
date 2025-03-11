402492ca22b009c3334d3f166d5d0bd3b961b58a
Gilzet <31349186+Gilzet@users.noreply.github.com>
2025-03-11 09:43:03 +0300

    fix(schema): default value from

--- db/migrate/20250311062123_add_tests_level_default.rb
+++ db/migrate/20250311062123_add_tests_level_default.rb
@@ -2,6 +2,6 @@
 
 class AddTestsLevelDefault < ActiveRecord::Migration[7.1]
   def change
-    change_column_default(:tests, :level, from: null, to: 0)
+    change_column_default(:tests, :level, from: nil, to: 0)
   end
 end
--- db/migrate/20250311062130_add_answers_correct_default.rb
+++ db/migrate/20250311062130_add_answers_correct_default.rb
@@ -2,6 +2,6 @@
 
 class AddAnswersCorrectDefault < ActiveRecord::Migration[7.1]
   def change
-    change_column_default(:answers, :correct, from: null, to: false)
+    change_column_default(:answers, :correct, from: nil, to: false)
   end
 end
