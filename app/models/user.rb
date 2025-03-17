7c8c7f5f3a0581a038ea8b941024bf9bd142810f
Gilzet <31349186+Gilzet@users.noreply.github.com>
2025-03-17 16:59:02 +0300

    fix(models): add author tests association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
+  has_many :tests, foreign_key: :author_id
 
   def passage_tests_by_level(level)
     tests.where(level: level)
