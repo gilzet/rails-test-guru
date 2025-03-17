0dbebfb90e42931163d6ef5014eba7dcf9aabb90
Gilzet <31349186+Gilzet@users.noreply.github.com>
2025-03-17 16:36:00 +0300

    refactor(models): update user inst method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
 
   def passage_tests_by_level(level)
-    Test.joins('JOIN "test_passages" ON "tests"."id" = "test_passages"."test_id"')
-        .where(level: level)
-        .where(test_passages: { user_id: id })
-        .pluck(:title)
+    tests.where(level: level)
+         .pluck(:title)
   end
 end
