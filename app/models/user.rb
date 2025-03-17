ba02df3b47d0d9740a7222f95bb162fbcf32eb02
Gilzet <31349186+Gilzet@users.noreply.github.com>
2025-03-17 13:56:24 +0300

    fix(models): rename User inst method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  def tests_by_level(level)
+  def passage_tests_by_level(level)
     Test.joins('JOIN "test_passages" ON "tests"."id" = "test_passages"."test_id"')
         .where(level: level)
         .where(test_passages: { user_id: id })
