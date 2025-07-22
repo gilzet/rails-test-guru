4e237d808e65dbbaa4a6cad8e6b196846b8d4a93
Gilzet <31349186+Gilzet@users.noreply.github.com>
2025-07-22 07:43:56 +0300

    style(models): change methods order

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :user, foreign_key: :author_id
+
   has_many :questions
   has_many :test_passages
   has_many :users, through: :test_passages
-  belongs_to :category
-  belongs_to :user, foreign_key: :author_id
 
   def self.names_by_category(title)
     Test.joins(:category)
